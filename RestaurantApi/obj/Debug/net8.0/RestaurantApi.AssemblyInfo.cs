7545510572d8ff6e3c35b536c598df4a7050c2b6
FZoltan40 <farkasz@kkszki.hu>
2025-12-08 09:06:41 +0100

    Models are created with Scaffold-DbContext command.

--- RestaurantApi/obj/Debug/net8.0/RestaurantApi.AssemblyInfo.cs
+++ RestaurantApi/obj/Debug/net8.0/RestaurantApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestaurantApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e3d263f360c94f74e0464c38284150f9a4af374")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestaurantApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RestaurantApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
